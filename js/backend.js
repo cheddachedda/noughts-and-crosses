592428f33144b15e14a65fa84c0afcad396d2659
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-04 23:33:08 +1100

    minor tweaks

--- js/backend.js
+++ js/backend.js
@@ -1,32 +1,40 @@
-const GAME = {
+const BACKEND = {
   board: [
     0, 0, 0,
     0, 0, 0,
-    0, 0, 0,
+    0, 0, 0
   ],
-
   moves: {
     1: [],
-    2: [],
+    2: []
   },
 
   currentPlayer: 1,   // either 1 or 2
 
-  play: function (cell) {
-    // Updates the game board
-    this.board[cell] = this.currentPlayer;
-    //
-    this.moves[this.currentPlayer].push(cell);
-    ////////////////////////
+  play: function (move) {
+    // Adds the move to the game board
+    this.board[move] = this.currentPlayer;
+
+    // Adds the move to the player's list of moves
+    this.moves[this.currentPlayer].push(move);
+
     console.log(
       `Player${this.currentPlayer}`,
-      cell,
-      logBoard(),
+      move,
+      this.logBoard(),
       this.checkForWin( this.moves[this.currentPlayer])
     );
-    ////////////////////////
-    this.currentPlayer += this.currentPlayer === 1 ? 1 : -1;
+
+    const didWin = this.checkForWin(this.moves[this.currentPlayer]);
+
+    if (didWin) {
+      this.winner = this.currentPlayer;
+    } else {
+      this.currentPlayer += this.currentPlayer === 1 ? 1 : -1;
+    }
   },
+
+  winner: null,
 
   winningCombos: [
     [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
@@ -38,21 +46,21 @@ const GAME = {
       if (won) return true;
     }
     return false;
+  },
+
+  // ONLY FOR TESTING
+  logBoard: function () {
+    console.log(BACKEND.board.slice(0, 3));
+    console.log(BACKEND.board.slice(3, 6));
+    console.log(BACKEND.board.slice(6));
   }
 };
 
-
-const logBoard = function () {
-  console.log(GAME.board.slice(0, 3));
-  console.log(GAME.board.slice(3, 6));
-  console.log(GAME.board.slice(6));
-};
-
-GAME.play(4);
-GAME.play(3);
-GAME.play(0);
-GAME.play(8);
-GAME.play(1);
-GAME.play(5);
-GAME.play(7);
+// BACKEND.play(4);
+// BACKEND.play(3);
+// BACKEND.play(0);
+// BACKEND.play(8);
+// BACKEND.play(1);
+// BACKEND.play(5);
+// BACKEND.play(7);
 // logBoard();
