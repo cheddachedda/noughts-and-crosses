e6f439a0df70ecff811d05cc2924e6c8e9377c0a
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-05 09:16:27 +1100

    refactor .board,  .play(),  .checkForWin()

--- js/backend.js
+++ js/backend.js
@@ -1,9 +1,10 @@
 const BACKEND = {
   board: [
-    0, 0, 0,
-    0, 0, 0,
-    0, 0, 0
+    '', '', '',
+    '', '', '',
+    '', '', ''
   ],
+
   moves: {
     1: [],
     2: []
@@ -12,25 +13,13 @@ const BACKEND = {
   currentPlayer: 1,   // either 1 or 2
 
   play: function (move) {
-    // Adds the move to the game board
-    this.board[move] = this.currentPlayer;
+    this.board[move] = this.currentPlayer;      // Adds the move to the game board
+    this.moves[this.currentPlayer].push(move);  // Adds the move to the player's list of moves
 
-    // Adds the move to the player's list of moves
-    this.moves[this.currentPlayer].push(move);
-
-    console.log(
-      `Player${this.currentPlayer}`,
-      move,
-      this.logBoard(),
-      this.checkForWin( this.moves[this.currentPlayer])
-    );
-
-    const didWin = this.checkForWin(this.moves[this.currentPlayer]);
-
-    if (didWin) {
-      this.winner = this.currentPlayer;
-    } else {
-      this.currentPlayer += this.currentPlayer === 1 ? 1 : -1;
+    if (this.checkForWin()) {                                   //  if player won,
+      this.winner = this.currentPlayer;                         //    assign a winner
+    } else {                                                    //  else
+      this.currentPlayer += this.currentPlayer === 1 ? 1 : -1;  //    change the current player
     }
   },
 
@@ -40,7 +29,8 @@ const BACKEND = {
     [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
   ],
 
-  checkForWin: function (moves) {
+  checkForWin: function () {
+    const moves = this.moves[this.currentPlayer];
     for (let combo of this.winningCombos) {
       const won = combo.every(n => moves.includes(n));
       if (won) return true;
@@ -55,12 +45,3 @@ const BACKEND = {
     console.log(BACKEND.board.slice(6));
   }
 };
-
-// BACKEND.play(4);
-// BACKEND.play(3);
-// BACKEND.play(0);
-// BACKEND.play(8);
-// BACKEND.play(1);
-// BACKEND.play(5);
-// BACKEND.play(7);
-// logBoard();
