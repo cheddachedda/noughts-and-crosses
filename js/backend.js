d7b17620dc6282f37865ed4b266577634ad8a826
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-06 12:38:12 +1100

    Refactor; prepare for scalability, bonus features

--- js/backend.js
+++ js/backend.js
@@ -1,57 +1,75 @@
-const BACKEND = {
+const GAME = {
   board: [
     '', '', '',
     '', '', '',
     '', '', ''
   ],
 
-  moves: {
-    1: [],
-    2: []
+  settings: {
+    boardSize: 3,
+    opponentType: 'Friend' // 'Easy', 'Medium', 'Impossible' or 'Friend'
+  },
+
+  players: {
+    1: {
+      token: 'X',
+      wins: 0,
+      moves: [],
+    },
+    2: {
+      token: 'O',
+      wins: 0,
+      moves: [],
+    },
   },
 
   currentPlayer: 1,   // either 1 or 2
 
-  play: function (move) {
-    this.board[move] = this.currentPlayer;      // Adds the move to the game board
-    this.moves[this.currentPlayer].push(move);  // Adds the move to the player's list of moves
+  play: function ( cell ) {
+    const p = this.currentPlayer;
+    // Adds the move to the game board
+    this.board[cell] = p;
+    // Adds the move to the player's list of moves
+    this.players[p].moves.push(cell);
 
     // Assigns a winner or draw if there is a result, else changes the current player.
     if (this.checkForWin()) {
       this.result = this.currentPlayer;
-    } else if (this.moves[1].length + this.moves[2].length === 9) {
-      this.result = 'Draw';
+      this.players[this.currentPlayer].wins++;
+    } else if (this.players[1].moves.length + this.players[2].moves.length === 9) {
+      this.result = 0;
     } else {
       this.currentPlayer += this.currentPlayer === 1 ? 1 : -1;
     }
   },
 
-  result: null,
+  checkForWin: function () {
+    const moves = this.players[this.currentPlayer].moves;
+    for (let combo of this.winningCombos) {
+      const won = combo.every(n => moves.includes(n));
+      if (won) {
+        return combo;
+      };
+    }
+    return false;
+  },
+
+  // TODO: Write a function that can get the winningCombos, based on board size.
+  getWinningCombos: function () {
+
+  },
 
   winningCombos: [
     [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
   ],
 
-  checkForWin: function () {
-    const moves = this.moves[this.currentPlayer];
-    for (let combo of this.winningCombos) {
-      const won = combo.every(n => moves.includes(n));
-      if (won) return true;
-    }
-    return false;
-  },
+  result: null, // null (still playing), 0 (draw), 1 (player 1) or 2 (player 2)
 
   reset: function () {
     this.board = [ '', '', '', '', '', '', '', '', '' ];
-    this.moves = { 1: [], 2: [] };
+    this.players[1].moves = [];
+    this.players[2].moves = [];
     this.currentPlayer = 1;
     this.result = null;
-  },
-
-  // ONLY FOR TESTING
-  logBoard: function () {
-    console.log(BACKEND.board.slice(0, 3));
-    console.log(BACKEND.board.slice(3, 6));
-    console.log(BACKEND.board.slice(6));
   }
 };
