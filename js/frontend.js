ef8a588a73fbf25873dcf2a795329cd3f0369f3d
daniel-nguyen <daniel.n.q.nguyen@gmail.com>
2021-10-06 12:41:48 +1100

    Remove modals

--- js/frontend.js
+++ js/frontend.js
@@ -1,9 +1,9 @@
-const renderBoard = function () {
-  for (let i = 0; i < BACKEND.board.length; i++) {
+const render = function () {
+  for (let i = 0; i < GAME.board.length; i++) {
     const cell = $(`#${i}`)
-    if (BACKEND.board[i] === 1) {
+    if (GAME.board[i] === 1) {
       cell.text('X');
-    } else if (BACKEND.board[i] === 2) {
+    } else if (GAME.board[i] === 2) {
       cell.text('O');
     } else {
       cell.text('');
@@ -12,30 +12,16 @@ const renderBoard = function () {
 };
 
 const handleClickCell = function (cell) {
-  BACKEND.play(cell);
-  renderBoard();
-  if (BACKEND.result) showModal();
-}
-
-const showModal = function () {
-  $('.modal-text').text(BACKEND.result);
-  $('.modal').css('display', 'block');
-};
-
-const closeModal = function () {
-  BACKEND.reset();
-  $('.modal').css('display', 'none');
-  $('.modal-text').text('');
-  renderBoard();
+  GAME.play(cell);
+  render();
+  if (GAME.result) showModal();
 }
 
 $(document).ready(function () {
+  render();
+
   $('.cell').on('click', function () {
     const id = Number($(this)[0].id);
     handleClickCell(id);
   });
-
-  $('#close-modal').on('click', function () {
-    closeModal();
-  })
 });
